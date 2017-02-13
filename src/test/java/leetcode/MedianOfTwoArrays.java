a05aca38c82cdc7e88f741381df36d35a679b200
liule2 <liule2@oocl.com>
2017-02-13 15:16:55 +0800

    Leetcode: Median of Two Arrays - 新的解法，时间复杂度为O(n)，空间复杂度为o(1)

--- src/test/java/leetcode/MedianOfTwoArrays.java
+++ src/test/java/leetcode/MedianOfTwoArrays.java
@@ -26,7 +26,7 @@ import edu.princeton.cs.algs4.StdOut;
 public class MedianOfTwoArrays {
     /**
      * 解法1：直接把两个数组结合，因为是有序数组，同时遍历两个数组，然后比较+结合
-     * 复杂度为O(n)，n是两个数组之和
+     * 时间复杂度为O(n)，n是两个数组之和，空间复杂度是O(n)
      *
      * @param nums1 有序数组
      * @param nums2 有序数组
@@ -69,6 +69,68 @@ public class MedianOfTwoArrays {
     }
 
     /**
+     * 解法2：还是用顺序查找，但是我们不需要使用O(n)的空间。
+     * 利用merge sort类似的原理，我们记录第K个大小的数，这样搜索到k时，直接返回。
+     * 时间复杂度时O(k)，空间复杂度时O(1)
+     *
+     * @param nums1 有序数组
+     * @param nums2 有序数组
+     * @return 中间值
+     */
+    private static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int k = (m + n + 1) / 2;
+
+        if ((m + n) % 2 == 0) {
+            return (getKthNum(nums1, m, nums2, n, k + 1) + getKthNum(nums1, m, nums2, n, k)) / 2.0f;
+        } else {
+            return getKthNum(nums1, m, nums2, n, k);
+        }
+
+    }
+
+    private static int getKthNum(int[] nums1, int m, int[] nums2, int n, int k) {
+        // 确保 m <= n
+        if (m > n) {
+            return getKthNum(nums2, n, nums1, m, k);
+        }
+
+        // 处理特殊情况
+        if (m == 0) {
+            return nums2[k - 1];
+        }
+        if (k == 1) {
+            return Math.min(nums1[0], nums2[0]);
+        }
+
+        int i = 0, j = 0;
+        int a1 = nums1[0], a2;
+        int result = 0;
+
+        while ((i + j) < k) {
+            if (i < m) a1 = nums1[i];
+            a2 = nums2[j];
+
+            if (a1 <= a2) {
+                // 确保数组不越界
+                if (i < m) {
+                    result = a1;
+                    i++;
+                } else {
+                    result = a2;
+                    j++;
+                }
+            } else if (a1 > a2) {
+                result = a2;
+                j++;
+            }
+        }
+
+        return result;
+    }
+
+    /**
      * 解法2：二分法查找，找到第K个大小的值，其中K是两个数组长m,n的一半。
      * 通过两次查找，如果m+n是偶数，则两次K是一样的；如果是奇数，则一次是K，一次K+1位置的值。
      * 两次的值的平均值就是中间值。
@@ -81,7 +143,7 @@ public class MedianOfTwoArrays {
      * @param nums2 有序数组
      * @return 中间值
      */
-    private static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+    private static double findMedianSortedArrays3(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
 
@@ -110,8 +172,8 @@ public class MedianOfTwoArrays {
 //    }
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 3};
-        int[] nums2 = {1};
+        int[] nums1 = {1, 2};
+        int[] nums2 = {3, 4};
         StdOut.println(findMedianSortedArrays(nums1, nums2));
         StdOut.println(findMedianSortedArrays2(nums1, nums2));
     }
